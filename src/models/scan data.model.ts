9fb5db73d73515d9e08c97055092627282f3031b
unknown <DAM229@ACARBALLEIRA.LOCAL>
2018-11-05 11:06:48 +0100

    Models

---
+++ src/models/scan data.model.ts
@@ -0,0 +1,10 @@
+export class ScanData{
+    info: string;//cadena de caracteres que devuelve la lectura QR
+    tipo: string; 
+
+    constructor(texto:string){
+        this.info=texto;
+        
+    }
+
+}
