9577f2512a321a8fe399147f0f3787c0087711ea
unknown <DAM229@ACARBALLEIRA.LOCAL>
2018-11-05 12:17:28 +0100

    Android

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private BarcodeScanner:BarcodeScanner) {
+  constructor(public navCtrl: NavController, private barcodeScanner:BarcodeScanner) {
 
+  }
+  escanear(){
+    console.log("realizando escaneo");
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     });
   }
 
 }
